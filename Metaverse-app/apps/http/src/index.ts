ac378c9759731dc93127a078dd2a9d860a7b2149
NickRaise <nikhilnainwaya17@gmail.com>
2025-05-28 19:44:53 +0530

    added the cors of express backend

--- Metaverse-app/apps/http/src/index.ts
+++ Metaverse-app/apps/http/src/index.ts
@@ -1,13 +1,15 @@
-import express from "express"
-import { V1Router } from "./routes/v1"
+import express from "express";
+import cors from "cors";
+import { V1Router } from "./routes/v1";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use("/api/v1", V1Router)
+app.use("/api/v1", V1Router);
 
 app.get("/", (req, res) => {
-    res.send("API working!!!")
-})
+  res.send("API working!!!");
+});
 
-app.listen(process.env.port || 3000)
+app.listen(process.env.port || 3000);
