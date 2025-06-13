168965cc137fb91b82b23a9374c4c37f5a05f1c5
NickRaise <nikhilnainwaya17@gmail.com>
2025-06-13 12:19:55 +0530

    removed the unnessary toggle logivc in the elelemtn menu

--- Metaverse-app/apps/frontend/components/custom/element-sidebar.tsx
+++ Metaverse-app/apps/frontend/components/custom/element-sidebar.tsx
@@ -20,7 +20,6 @@ const AllElementsMenu = ({
   setElement: Dispatch<SetStateAction<Element | null>>;
 }) => {
   const [allElements, setAllElements] = useState<Element[]>();
-  const [isOpen, setIsOpen] = useState(true);
 
   const fetchAllElements = async () => {
     try {
@@ -37,61 +36,37 @@ const AllElementsMenu = ({
 
   return (
     <div className="sticky top-0 left-0 z-50">
-      <div
-        className={clsx(
-          " h-screen bg-custom-bg-dark-2 overflow-y-scroll transition-all duration-300 relative",
-          isOpen ? "w-36" : "w-0"
-        )}
-      >
-        {isOpen && (
-          <>
-            <div className="my-3 text-center text-custom-text-primary font-semibold text-md">
-              <button className="text-custom-accent hover:underline cursor-pointer hover:text-custom-highlight transition duration-150">
-                ← Go Back
-              </button>
-            </div>
-
-            <div className="flex items-center">
-              <div className="flex gap-3 px-4 flex-wrap items-center justify-center">
-                {allElements?.map((e) => (
-                  <div className="flex-shrink-0" key={e.id}>
-                    <Image
-                      src={e.imageUrl}
-                      width={e.width * TILE_SIZE}
-                      height={e.height * TILE_SIZE}
-                      onClick={() =>
-                        setElement((state) => (state?.id !== e.id ? e : null))
-                      }
-                      alt="element image"
-                      className={clsx(
-                        "p-2 bg-custom-bg-dark-1 rounded-lg cursor-pointer hover:scale-105 transition-all duration-200 hover:shadow-[rgba(0,173,181,0.6)] shadow-sm max-h-[90px] object-contain",
-                        element?.id === e.id
-                          ? "border-2 border-custom-border-highlight"
-                          : "border-2 border-transparent"
-                      )}
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </>
-        )}
-      </div>
-      {/* <div className="w-full flex justify-center">
-        <div
-          onClick={() => setIsOpen((prev) => !prev)}
-          className="w-28 h-16 text-custom-text-primary bg-custom-bg-dark-2 transition-all duration-200 flex items-center justify-center cursor-pointer -mt-5 hover:bg-gradient-to-b hover:from-custom-bg-dark-2 hover:to-custom-bg-dark-1 border border-custom-bg-dark-2"
-          style={{
-            clipPath: "polygon(0.87% 31.21%, 99.13% 31.21%, 75% 75%, 25% 75%)",
-          }}
-        >
-          {isOpen ? (
-            <X className="w-4 h-4 mt-1" />
-          ) : (
-            <ChevronDown className="w-4 h-4" />
-          )}
+      <div className="h-screen w-36 bg-custom-bg-dark-2 overflow-y-scroll overflow-x-hidden transition-all duration-300 relative">
+        <div className="my-3 text-center text-custom-text-primary font-semibold text-md">
+          <button className="text-custom-accent hover:underline cursor-pointer hover:text-custom-highlight transition duration-150">
+            ← Go Back
+          </button>
         </div>
-      </div> */}
+
+        <div className="flex items-center">
+          <div className="flex gap-3 px-4 flex-wrap items-center justify-center">
+            {allElements?.map((e) => (
+              <div className="flex-shrink-0" key={e.id}>
+                <Image
+                  src={e.imageUrl}
+                  width={e.width * TILE_SIZE}
+                  height={e.height * TILE_SIZE}
+                  onClick={() =>
+                    setElement((state) => (state?.id !== e.id ? e : null))
+                  }
+                  alt="element image"
+                  className={clsx(
+                    "p-2 bg-custom-bg-dark-1 rounded-lg cursor-pointer hover:scale-105 transition-all duration-200 hover:shadow-[rgba(0,173,181,0.6)] shadow-sm max-h-[90px] object-contain",
+                    element?.id === e.id
+                      ? "border-2 border-custom-border-highlight"
+                      : "border-2 border-transparent"
+                  )}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
